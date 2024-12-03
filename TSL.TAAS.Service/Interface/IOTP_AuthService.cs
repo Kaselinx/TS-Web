4e46d837b904d907f23380149c2c3df2720895d1
RonnyLin_x <ronnylin_x@hotmail.com>
2024-12-03 10:51:51 +0800

    commit

---
+++ TSL.TAAS.Service/Interface/IOTP_AuthService.cs
@@ -0,0 +1,25 @@
+﻿
+using TSL.Base.Platform.Utilities;
+using TSL.Common.Model.Service.OTP;
+
+namespace TSL.TAAA.Service.Interface
+{
+    public interface IOTP_AuthService
+    {
+        /// <summary>
+        /// if the OTP_AUTH data exists update the data, if not insert the data
+        /// </summary>
+        /// <param name="otpAuth"></param>
+        /// <returns></returns>
+        Task<ServiceResult<int>> UpdateInsertOTPData(OTP_AUTHServiceModel otpAuth);
+
+        /// <summary>
+        /// check if OTP
+        /// </summary>
+        /// <param name="seqNo"></param>
+        /// <param name="systemID"></param>
+        /// <param name="oTP"></param>
+        /// <returns></returns>
+        Task<ServiceResult<bool>> CheckOTPData(int seqNo, string systemID, string oTP);
+    }
+}
---
+++ TSL.TOTP.Model.DataAccessLayer/OTP/OTP_AUTH.cs
@@ -0,0 +1,46 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace TSL.Common.Model.DataAccessLayer.OTP
+{
+    [Table("OTP_AUTH")]
+    public class OTP_AUTH
+    {
+        /// <summary>
+        /// primary key
+        /// </summary>
+        [Key]
+        public int SeqNo { get; set; }
+
+        /// <summary>
+        /// System identifier
+        /// </summary>
+        public required string SystemID { get; set; }
+
+        // Telephone number
+        public required string Tel_No { get; set; }
+
+        // Email address
+        public required string Mail { get; set; }
+
+        // One-time password
+        public required string OTP { get; set; }
+
+        // Status of the OTP
+        public required string Status { get; set; }
+
+        // Creation time of the OTP
+        public DateTime? CreateTime { get; set; }
+
+        // End time of the OTP
+        public DateTime? EndTime { get; set; }
+
+        // Effective duration in seconds
+        public int? Effect_Second { get; set; }
+
+        // Status modification date
+        public DateTime? Status_ModiDate { get; set; }
+
+        // Source IP address
+        public required string SourceIP { get; set; }
+    }
+}
